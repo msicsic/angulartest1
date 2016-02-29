c2800436af07d5d3712036276961001a1581744d
msicsic <msicsic@yahoo.com>
2016-02-29 20:49:10 +0100

    hop

--- js/mineSweep.js
+++ js/mineSweep.js
@@ -1,7 +1,9 @@
 var minesweepApp = angular.module('minesweepApp', []);
 
 minesweepApp.controller('fieldCtrl', ["$scope", ($scope) => { 
-	$scope.score = Math.random()*100;
+	$scope.score = Math.random()*100
+
+	$scope.change = () => $scope.score += 1
 	}]
 );
 
